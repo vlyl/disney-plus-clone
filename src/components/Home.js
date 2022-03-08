dc55c07c8da3a7894aed182434bc11830b196cb8
shannon <liyunlong518@gmail.com>
2022-03-09 00:28:03 +0800

    image slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
 
 function Home() {
   return (
-    <Container>Home</Container>
+    <Container>
+      <ImgSlider />
+    </Container>
   )
 }
 
