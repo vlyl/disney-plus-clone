749480b50c823a06976e265d979e045c51e0dcde
shannon <liyunlong518@gmail.com>
2022-03-13 22:56:12 +0800

    detail

---
+++ src/components/Detail.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img alt='' src='https://cdn.vox-cdn.com/thumbor/cLeR3YH-thYtT160I5lWtLpYXeE=/0x0:4096x2304/1820x1213/filters:focal(1973x1175:2627x1829):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60190709/BO_RGB_s120_22a_cs_pub.pub16.318.0.jpg' />
+      </Background>
+      <ImageTitle>
+        <img alt='' src='https://images.fanart.tv/fanart/bao-5e7d7d556c45a.png' />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img alt='' src='/images/play-icon-black.png' />
+          <span>PLAY</span>
+
+        </PlayButton>
+        <TrailerButton>
+          <img alt='' src='/images/play-icon-white.png' />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img alt='' src='/images/group-icon.png' />
+        </GroupWatchButton>
+      </Controls>
+      <Subtitle>
+        Subtitle
+      </Subtitle>
+      <Description>
+        Description
+      </Description>
+    </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+  
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(198, 198, 198);
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgba(0, 0, 0);
+`
+
+const Subtitle = styled.div`
+  color: white;
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: white;
+`
